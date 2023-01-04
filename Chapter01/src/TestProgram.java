6f16dc26759276fc5f9ee3bcd506fd5d0158ceb8
Rick Halterman <halterman@southern.edu>
2023-01-04 10:46:34 -0500

    Second push

--- Chapter01/src/TestProgram.java
+++ Chapter01/src/TestProgram.java
@@ -2,7 +2,7 @@
 public class TestProgram {
 
 	public static void main(String[] args) {
-		System.out.println("This is a test.");
+		System.out.println("This is a test for the MATH 320 repo.");
 
 	}
 
