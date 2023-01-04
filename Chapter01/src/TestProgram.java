e7a6b6d523c5b89fb874fdf3aade38fe6292ee31
Rick Halterman <halterman@southern.edu>
2023-01-04 10:48:52 -0500

    Third push

--- Chapter01/src/TestProgram.java
+++ Chapter01/src/TestProgram.java
@@ -2,7 +2,7 @@
 public class TestProgram {
 
 	public static void main(String[] args) {
-		System.out.println("This is a test for the MATH 320 repo.");
+		System.out.println("This is a test for the MATH 320 repository.");
 
 	}
 
